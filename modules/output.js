64c0f4ab06e0661571ecbb6b2ec28b05058f0c87
massimocode <info@massimoahmadi.com>
2016-06-20 19:08:42 +0100

    Fix for debug mode and improve readme. See below:
    - Fixed an issue where the debug mode would create a new output window for every log entry it made. Now they are appending in one window as expected. Also show output window the first time it is written to.
    - Add documentation for configuration options.
    - Fix a few small typos in readme.

--- modules/output.js
+++ modules/output.js
@@ -1,10 +1,15 @@
 var vscode = require("vscode");
 var _ = require("lodash");
 
+var outputChannel;
+
 module.exports = function(msg) {
     if(_.startsWith(msg, "[connection] > 'PASS"))
         return;
 
-    var output = vscode.window.createOutputChannel("ftp-sync");
-    output.appendLine(msg);
+    if (outputChannel === undefined) {
+        outputChannel = vscode.window.createOutputChannel("ftp-sync");
+        outputChannel.show();
+    }
+    outputChannel.appendLine(msg);
 }
