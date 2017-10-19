7717fe0d8b34b612ab89d8db3498fc463ec0f0c4
Donovan Hutchence <donovan.hutchence@gmail.com>
2017-10-19 11:31:27 +0100

    fix bug where ignore and allow patters are incorrectly treated as unix paths

--- modules/is-ignored.js
+++ modules/is-ignored.js
@@ -5,10 +5,6 @@ module.exports = function(path, patterns_allowed, patterns_ignored) {
 
 	var n = 0, skip = false;
 	if (patterns_allowed.length > 0) {
-		patterns_allowed = patterns_allowed.map(function(p) {
-			return upath.toUnix(p);
-		});
-
 		skip = true;
 		for (n = 0; n < patterns_allowed.length; n++){
 			if ((new RegExp(patterns_allowed[n])).test(path)) {
@@ -18,9 +14,6 @@ module.exports = function(path, patterns_allowed, patterns_ignored) {
 		}
 	}
 
-	patterns_ignored = patterns_ignored.map(function(p) {
-		return upath.toUnix(p);
-	});
 	for (n = 0; n < patterns_ignored.length; n++){
 		if ((new RegExp(patterns_ignored[n])).test(path)) {
 			skip = true;
