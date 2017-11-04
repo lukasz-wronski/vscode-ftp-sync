dd57554ff6689419df1603c185b1e3ae2bf800fa
shty <Page.qy@test.test>
2017-11-04 16:05:14 +0800

    add list command

--- modules/list-command.js
+++ modules/list-command.js
@@ -6,9 +6,7 @@ var isIgnored = require('./is-ignored');
 var output = require('./output');
 const downloadFn = require('./downloadcurrent-command');
 const UploadFn = require('./uploadcurrent-command');
-let mygetFtpSync = null;
 module.exports = function(fileUrl, getFtpSync) {
-  mygetFtpSync = getFtpSync;
   if (!vscode.workspace.rootPath) {
     vscode.window.showErrorMessage(
       'Ftp-sync: Cannot init ftp-sync without opened folder'
@@ -33,67 +31,108 @@ module.exports = function(fileUrl, getFtpSync) {
   console.log('fileUrl:', fileUrl);
   console.log('config:', config);
   output('fileUrl' + JSON.stringify(fileUrl));
-  let remotePath = fileUrl.fsPath
-    .replace(vscode.workspace.rootPath, config.remotePath)
-    .split('/');
-  remotePath = remotePath.slice(0, remotePath.length - 1).join('/');
+  let remotePath = getFatherPath(
+    fileUrl.fsPath.replace(vscode.workspace.rootPath, config.remotePath)
+  );
   console.log('remotePath:', remotePath);
-  getFtpSync().toListRemoteFiles(remotePath, function(err, files) {
-    if (err) {
-      console.error('err:', err);
-      vscode.window.showErrorMessage('Ftp-sync: Listing failed: ' + err);
-    } else {
-      vscode.window.setStatusBarMessage(
-        'Ftp-sync: Listing successfully!',
-        STATUS_TIMEOUT
-      );
-      console.log('files:', files);
-      showFiles(files, getFtpSync);
-    }
-  });
+  function listAllFiles(filesRemotePath) {
+    getFtpSync().toListRemoteFiles(filesRemotePath, function(err, files) {
+      if (err) {
+        console.error('err:', err);
+        vscode.window.showErrorMessage('Ftp-sync: Listing failed: ' + err);
+      } else {
+        vscode.window.setStatusBarMessage(
+          'Ftp-sync: Listing successfully!',
+          STATUS_TIMEOUT
+        );
+        console.log('files:', files);
+        showFiles(files, filesRemotePath);
+      }
+    });
+  }
+  listAllFiles(remotePath);
+  function showFiles(files, filesRemotePath) {
+    const pickOptions = files.map(file => ({
+      label: getLabel(file),
+      description: file.path,
+      file,
+      isDir: file.isDir
+    }));
+    const pickResult = vscode.window.showQuickPick(
+      [
+        {
+          label: '../',
+          description: '. UP a folder',
+          backPath: getFatherPath(filesRemotePath)
+        }
+      ].concat(pickOptions),
+      {
+        placeHolder: 'Select a folder or file'
+      }
+    );
+
+    pickResult.then(function(result) {
+      if (!result) {
+        return;
+      }
+      console.log('sel file:', result);
+      if (result.backPath) {
+        listAllFiles(result.backPath);
+      } else if (result.isDir) {
+        listAllFiles(result.file.path);
+      } else {
+        showFileActions(result.file);
+      }
+    });
+  }
+  function showFileActions(file) {
+    const pickOptions = [
+      {
+        label: 'DownLoad',
+        description: 'DownLoad this file',
+        file,
+        operation: 'download'
+      },
+      {
+        label: 'Upload',
+        description: 'Upload this file',
+        file,
+        operation: 'upload'
+      }
+    ];
+    const pickResult = vscode.window.showQuickPick(pickOptions);
+
+    pickResult.then(function(result) {
+      console.log('sel Actions:', result);
+      if (!result) {
+        return;
+      }
+      if (result.operation === 'download') {
+        downloadFn(getLocalPath(result.file.path), getFtpSync);
+      } else if (result.operation === 'upload') {
+        UploadFn(getLocalPath(result.file.path), getFtpSync);
+      }
+    });
+  }
 };
-function showFiles(files) {
-  const pickOptions = files.map(file => ({
-    label:
-      file.name && file.name.indexOf('/') === 0
-        ? file.name.slice(1)
-        : file.name,
-    description: file.path,
-    file
-  }));
-
-  const pickResult = vscode.window.showQuickPick(pickOptions, {
-    placeHolder: 'All files:'
-  });
-
-  pickResult.then(function(result) {
-    console.log('sel file:', result);
-    showActions(result.file);
-  });
+function getLabel(file) {
+  if (file.isDir) {
+    return `${file.name}/`;
+  } else
+    return file.name && file.name.indexOf('/') === 0
+      ? file.name.slice(1)
+      : file.name;
 }
-function showActions(file) {
-  const pickOptions = [
-    {
-      label: 'DownLoad',
-      description: 'DownLoad this file',
-      file,
-      operation: 'download'
-    },
-    {
-      label: 'Upload',
-      description: 'Upload this file',
-      file,
-      operation: 'upload'
-    }
-  ];
-  const pickResult = vscode.window.showQuickPick(pickOptions);
-
-  pickResult.then(function(result) {
-    console.log('sel Actions:', result);
-    if (result.operation === 'download') {
-      downloadFn(result.file, mygetFtpSync);
-    } else if (result.operation === 'upload') {
-      UploadFn(result.file, mygetFtpSync);
-    }
-  });
+function getLocalPath(fileRemotePath) {
+  return {
+    fsPath: fileRemotePath.replace(
+      ftpconfig.getConfig().remotePath,
+      vscode.workspace.rootPath
+    )
+  };
+}
+function getFatherPath(son) {
+  let father = son.split('/');
+  father = father.slice(0, father.length - 1).join('/');
+  return father;
 }
